24209dff810c81a7a900fe56218381e8fdea6545
Lee Santos <lee.m.santos@outlook.com>
2022-10-17 17:53:54 -0400

    Completed tasks 3 to 6.

--- week_01/we/chinook_exercise.sql
+++ week_01/we/chinook_exercise.sql
@@ -48,21 +48,66 @@ SELECT DISTINCT artists.Name AS 'Artist' FROM artists
 Which video track has the longest length?
 media_types.MediaTypeId IS 3 for video, tracks.Milliseconds, */
 
-
+SELECT tracks.Name, media_types.Name, tracks.Milliseconds AS 'Duration' FROM media_types
+  JOIN tracks ON tracks.MediaTypeId=media_types.MediaTypeId
+  WHERE media_types.Name LIKE '%video%'
+  ORDER BY Duration DESC
+  LIMIT 1;
 
 /* TASK IV
-Find the names of customers who live in the same city as the top employee (The one not managed by anyone).
+Find the names of customers who live in the same city as the top employee
+(The one not managed by anyone).
 */
 
+SELECT
+  customers.FirstName AS 'CustFirst',
+  customers.LastName AS 'CustLast',
+  customers.City AS 'CustCity',
+  employees.FirstName AS 'EmpFirst',
+  employees.LastName AS 'EmpLast',
+  employees.City AS 'EmpCity'
+FROM customers, employees
+  WHERE ReportsTo IS NULL
+    AND CustCity=EmpCity;
 
+/* Check the customer directory for customers living in Edmonton */
+SELECT * FROM customers
+WHERE customers.City='Edmonton';
 
 /* TASK V
 Find the managers of employees supporting Brazilian customers.
 */
 
+SELECT
+  A.FirstName AS 'EmpFirst',
+  A.LastName AS 'EmpLast',
+  A.Title AS 'Title',
+  A.ReportsTo AS 'ReportsTo',
+  customers.FirstName AS 'CustFirst',
+  customers.LastName AS 'CustLast',
+  customers.City AS 'City',
+  customers.Country AS 'Country'
+FROM employees AS 'A', employees AS 'B'
+JOIN customers ON A.EmployeeId=customers.SupportRepId
+WHERE customers.Country='Brazil';
 
+SELECT
+  A.FirstName AS 'EmpFirst',
+  A.LastName AS 'EmpLast',
+  A.Title AS 'Title',
+  B.FirstName AS 'MngrFirst',
+  B.LastName As 'MngrLast',
+  customers.Country AS 'SupportedCountry'
+FROM employees AS 'A', employees AS 'B'
+JOIN customers ON A.EmployeeId=customers.SupportRepId
+WHERE customers.Country='Brazil' AND A.ReportsTo=B.EmployeeId;
 
 /* TASK VI
 Which playlists have no Latin tracks?
 */
 
+SELECT DISTINCT PlaylistId FROM playlist_track
+JOIN tracks ON tracks.TrackId=playlist_track.TrackId
+JOIN genres ON genres.GenreId=tracks.GenreId
+WHERE genres.Name IS NOT 'Latin'
+ORDER BY PlaylistId;
